5a0b2b869ac00eebe0a760e55abfaba1baf92a62
Ben Roberts <bendando@hotmail.co.uk>
2023-04-25 17:44:24 +0100

    added SSR support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,18 +25,26 @@ export default function Home({ story }: any): JSX.Element {
   );
 }
 
+
+
+
 type sbParamsType = {
   version: "draft" | "published" | undefined
 };
  
-export async function getStaticProps() {
-  // home is the default slug for the homepage in Storyblok
+export async function getServerSideProps(context: any) {
+  // get the query object
+  const insideStoryblok = context.query._storyblok;
+ 
   let slug = "home";
  
-  // load the draft version
-  let sbParams: sbParamsType = {
-    version: "draft", // or 'published'
+  let sbParams : sbParamsType = {
+    version: "published", // or 'draft'
   };
+ 
+  if (insideStoryblok) {
+    sbParams.version = "draft";
+  }
  
   const storyblokApi = getStoryblokApi();
   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
@@ -46,6 +54,26 @@ export async function getStaticProps() {
       story: data ? data.story : false,
       key: data ? data.story.id : false,
     },
-    revalidate: 3600, // revalidate every hour
   };
 }
+
+// export async function getStaticProps() {
+//   // home is the default slug for the homepage in Storyblok
+//   let slug = "home";
+ 
+//   // load the draft version
+//   let sbParams: sbParamsType = {
+//     version: "draft", // or 'published'
+//   };
+ 
+//   const storyblokApi = getStoryblokApi();
+//   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+ 
+//   return {
+//     props: {
+//       story: data ? data.story : false,
+//       key: data ? data.story.id : false,
+//     },
+//     revalidate: 3600, // revalidate every hour
+//   };
+// }
