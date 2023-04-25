e489701bbee5116550f62eb65e7a1ab249872972
Ben Roberts <bendando@hotmail.co.uk>
2023-04-25 15:21:31 +0100

    storyblok pulling in name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+ 
+storyblokInit({
+  accessToken: "JqynnGEuwXQVlAQgcosJigtt",
+  use: [apiPlugin],
+  apiOptions: {
+    region: "eu",
+  },
+});
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+    
